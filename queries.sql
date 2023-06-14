c7b8e42ade0c436227dff95b7a27b24ccff3aaf5
Osman Alhassan <oalhassan847@gmail.com>
2023-06-14 04:22:46 +0000

    Query names of animals born between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
