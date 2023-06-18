3b4a0ae4878d73533944517a2b29bd2325e3322c
Osman Alhassan <oalhassan847@gmail.com>
2023-06-18 00:41:39 +0000

    Enter data for specialization

--- data.sql
+++ data.sql
@@ -64,3 +64,9 @@ VALUES ('William Tatcher',45,'2000-04-23'),
 ('Maisy Smith',26,'2019-01-17'),
 ('Stephanie Mendez',64,'1981-05-04'),
 ('Jack Harkenss',38,'2008-06-01');
+
+INSERT INTO specialization
+VALUES (1, 1),
+(1, 3),
+(2, 3),
+(2, 4);
