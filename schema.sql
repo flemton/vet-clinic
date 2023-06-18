8bfe5c70fe93494ae3dc8a73b1240e7b9bba4c54
Osman Alhassan <oalhassan847@gmail.com>
2023-06-18 00:40:24 +0000

    Create a "join table" called visits

--- schema.sql
+++ schema.sql
@@ -45,3 +45,10 @@ CREATE TABLE specialization (
     vet_id              INT REFERENCES vets (id) ON UPDATE CASCADE,
     PRIMARY KEY (species_id, vet_id)
 );
+
+CREATE TABLE visits (
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES  vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY (animal_id, vet_id, date_of_visit)
+); 
